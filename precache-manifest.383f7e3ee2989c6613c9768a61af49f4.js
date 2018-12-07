92c558e9a1dfae6b2dc244ca1c7be88f1c26319f
sreekar <sreekar339@gmail.com>
2018-12-07 05:57:37 +0530

    Updates

---
+++ precache-manifest.383f7e3ee2989c6613c9768a61af49f4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a536838eb4efdf701fd3a5419b72ce61",
+    "url": "/xoxo-multiplayer/static/media/ThreeBallsLoader.a536838e.svg"
+  },
+  {
+    "revision": "9d4616929e9d7efe968ee49afe52006c",
+    "url": "/xoxo-multiplayer/static/media/BarsLoader.9d461692.svg"
+  },
+  {
+    "revision": "fcd0c8aae4f618527591",
+    "url": "/xoxo-multiplayer/static/js/runtime~main.fcd0c8aa.js"
+  },
+  {
+    "revision": "b775af1a59565c598563",
+    "url": "/xoxo-multiplayer/static/js/main.b775af1a.chunk.js"
+  },
+  {
+    "revision": "c784250ab7a8db0d14b4",
+    "url": "/xoxo-multiplayer/static/js/1.c784250a.chunk.js"
+  },
+  {
+    "revision": "87ab880d281423dbbdc7b796db877bd5",
+    "url": "/xoxo-multiplayer/index.html"
+  }
+];
